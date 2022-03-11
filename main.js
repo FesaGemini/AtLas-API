196464051b17587b24c47db2c6f2f1dce46275ec
dano <92238288+Dano57@users.noreply.github.com>
2022-03-11 16:33:14 +0530

    Update main.js

--- main.js
+++ main.js
@@ -2,13 +2,11 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-    res.send("AtLas-API")
+    res.redirect("https://youtu.be/dQw4w9WgXcQ")
 })
 
 app.get("/facts", (req, res) => {
-
     let array = require("./src/json/facts.json").facts;
     res.json({ fact: `${array[~~(Math.random() * array.length)]}` })
 });
-
 app.listen(3000, () => console.log("Ready To Rock'en Roll"));
