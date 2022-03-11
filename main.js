354dc08505b8e9b339554b1bb965682b9e1fe525
dano <92238288+Dano57@users.noreply.github.com>
2022-03-11 16:02:39 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
-const { readFileSync } = require("fs");
 const express = require("express");
 const app = express();
 
+app.get("/", (req, res) => {
+    res.send("AtLas-API")
+})
 
 app.get("/facts", (req, res) => {
 
-    let path = readFileSync("./src/json/facts.json");
-    let obj = JSON.parse(path);
-
-    res.json({ fact: `${obj[Math.floor(Math.random() * obj.length)]}` })
+    let array = require("./src/json/facts.json").facts;
+    res.json({ fact: `${array[~~(Math.random() * array.length)]}` })
 });
 
 app.listen(3000, () => console.log("Ready To Rock'en Roll"));
