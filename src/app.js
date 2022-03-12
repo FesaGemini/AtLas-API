5d703dd88a3c7349d36c7a54d2966a1efb50869f
pratik12350 <67191233+pratik12350@users.noreply.github.com>
2022-03-12 20:48:04 +0530

    Added Request logging system
    Added request logging with `Morgan` use `npm i morgan` and install it.

--- src/app.js
+++ src/app.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const app = express();
+const morgan = require('morgan');
+
 app.use(express.json());
 app.set('json spaces', 2);
 app.use(require("./router"))
+app.use(morgan('dev'))
 
 app.listen(3000, async () => {
   console.log("[SERVER] AtLas-API V1 Ready")
