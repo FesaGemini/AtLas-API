0e4adbcaec696c73714810187c0790f8a7a1dc0f
Pwetty Pwincess <86605082+CatgirlsSimp@users.noreply.github.com>
2022-03-14 09:57:08 +0400

    Create 8ball.js

---
+++ src/routes/8ball.js
@@ -0,0 +1,15 @@
+module.exports = {
+ route: "/8ball",
+ run: async(req, res) => {
+   
+ const question = req.query.question;
+ if (!question) return res.json({ error: "Ask me something" });
+ 
+  const ball = ["I think so", "Yes", "No", "I guess", 'Maybe', "Good for you", "I'm just built differently" ,"Definitely", "Definitely Not", "I think so", "I dont think so", "I Didnt Ask", "Outlook good", "I dont care"]
+  
+  const response = ball[Math.floor(Math.random() * ball.length)];
+
+  return res.status(200).json({ message: response })
+   
+ }
+}
